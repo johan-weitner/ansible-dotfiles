8588eabf23ee8caa8c591c53ac16a5120ffab62a
johanweitner@gmail.com <johanweitner@gmail.com>
2024-06-10 22:24:33 +0200

    WiP

--- test-docker.sh
+++ test-docker.sh
@@ -7,10 +7,10 @@ docker build -t my-ansible-image .
 docker run -d --name my-ansible-container my-ansible-image
 
 # # Copy the playbook to the container
-# docker cp playbook.yml my-ansible-container:/etc/ansible
+docker cp playbook.yml my-ansible-container:/etc/ansible
 
 # # Run the playbook in the container
-# docker exec -it my-ansible-container ansible-playbook /etc/ansible/playbook.yml
+docker exec -it my-ansible-container ansible-playbook /etc/ansible/playbook.yml
 
 # # Clean up
 # docker rm -f my-ansible-container
